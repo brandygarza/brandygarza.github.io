826d39e7b42c7f2c51e91f436a3cf1a46005b289
Garza <300079729@fvtc.edu>
2024-05-16 12:24:01 -0500

    added welcome div

--- site.js
+++ site.js
@@ -1,38 +1,31 @@
 //assignment 4 - secret message 
-const secretMessage = "This is a secret message!";
+// const secretMessage = "This is a secret message!";
 
-localStorage.setItem("It's a secret to everybody.", secretMessage);
-if (secretMessage) {
-  console.log("Secret Message:", secretMessage);
-} else {
-  console.log("Secret message not received.");
-}
+// localStorage.setItem("It's a secret to everybody.", secretMessage);
+// if (secretMessage) {
+//   console.log("Secret Message:", secretMessage);
+// } else {
+//   console.log("Secret message not received.");
+// }
 //assignment 3 - dynamic welcome
-// const hours = new Date().getHours() 
-// function Morning(){
-//     const isMorning = hours >= 4 && hours < 12
-//     console.log("Good Morning!")
-// }
-// function Afternoon(){
-//     const isAfternoon = hours >= 12 && hours < 17
-//     console.log("Good Afternoon!")
-// }
-// function Evening(){
-//     const isEvening = hours >= 17 || hours < 4
-//     console.log("Good Evening!")
-// }
-// function welcomeBox(){
-// }
-//const timeMessage = document.querySelectorAll('.welcome')
-//if(Morning()){
-  //  console.log("Good Morning!")
-    //}
-    //if(Afternoon()){
- //       console.log("Good Afternoon!")
-   // }
- //   if (Evening()){
-  //      console.log("Good Evening!")
-   // }
+
+ const hours = new Date().getHours()
+
+ const isMorning = hours >= 4 && hours < 12 //morning?
+ const isAfternoon = hours >= 12 && hours < 17 //afternoon?
+ const isEvening = hours >= 17 || hours < 4 //evening?
+
+ 
+ const h1 = welcome.querySelector('h1')
+ if(isMorning){
+  h1.textcontent = "Good Morning!"
+ }
+ if(isAfternoon){
+  h1.textcontent = "Good Afternoon!"
+ }
+ if(isEvening){
+  h1.textcontent = "Good Evening!"
+ }
 
 
 
